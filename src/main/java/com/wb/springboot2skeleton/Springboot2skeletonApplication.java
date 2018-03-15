bc1969453dd09877394824752ff3a648010be74d
13889202209 <526477565@qq.com>
2018-03-15 17:15:21 +0800

    111

--- src/main/java/com/wb/springboot2skeleton/Springboot2skeletonApplication.java
+++ src/main/java/com/wb/springboot2skeleton/Springboot2skeletonApplication.java
@@ -2,11 +2,12 @@ package com.wb.springboot2skeleton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Springboot2skeletonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Springboot2skeletonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2skeletonApplication.class, args);
+    }
 }
