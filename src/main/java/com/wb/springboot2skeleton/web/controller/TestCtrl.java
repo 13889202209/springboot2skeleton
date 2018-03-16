ec5cbfe84b6bacf012b6b7343e99b14b0fa9aa05
13889202209 <526477565@qq.com>
2018-03-16 09:18:37 +0800

    2222

--- src/main/java/com/wb/springboot2skeleton/web/controller/TestCtrl.java
+++ src/main/java/com/wb/springboot2skeleton/web/controller/TestCtrl.java
@@ -1,4 +1,4 @@
-package com.wb.springboot2skeleton.controller;
+package com.wb.springboot2skeleton.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
